7f002c152ac1720aa58f9f752411bd9619e5ca18
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2022-08-22 15:27:14 +0200

    Fandt et Hack online, men ærligt, forstår ikke den der (bot) => hvordan det fungerer så det er nok lidt snyd. Det er Node kan jeg se

---
+++ oevelse_1/script.js
@@ -0,0 +1,17 @@
+const botle = document.querySelectorAll(".flaske");
+
+botle.forEach((bot) => {
+  bot.addEventListener("click", alkoholTjek);
+
+  function alkoholTjek() {
+    bot.removeEventListener("click", alkoholTjek);
+
+    if (bot.classList.contains("alkohol")) {
+      console.log("Der er alkohol i denne vare.");
+    } else {
+      console.log("Der er ikke alkohol i denne vare.");
+    }
+
+    bot.addEventListener("click", alkoholTjek);
+  }
+});
